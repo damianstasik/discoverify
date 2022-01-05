f4b2003d94b68c75035a52c48801053571755591
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-01-05 13:56:41 +0100

    Clean up list of genres from all followed artists

--- src/routes/FollowedArtistsGenres.tsx
+++ src/routes/FollowedArtistsGenres.tsx
@@ -1,38 +1,11 @@
 import Button from '@mui/material/Button';
 import { Link as RouterLink } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import PersonAddTwoToneIcon from '@mui/icons-material/PersonAddTwoTone';
-import LoadingButton from '@mui/lab/LoadingButton';
-import Skeleton from '@mui/material/Skeleton';
 import { GridColumns, DataGridPro } from '@mui/x-data-grid-pro';
 import { useAtomValue } from 'jotai/utils';
 import { useQuery } from 'react-query';
 import { tokenIdState } from '../store';
 import { Layout } from '../components/Layout';
-
-function ArtistCardSkeleton() {
-  return (
-    <Card>
-      <Skeleton variant="rectangular" height={300} animation="wave" />
-      <CardContent>
-        <Typography variant="h6">
-          <Skeleton animation="wave" />
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <Skeleton variant="rectangular" animation="wave">
-          <LoadingButton startIcon={<PersonAddTwoToneIcon />}>
-            Follow
-          </LoadingButton>
-        </Skeleton>
-      </CardActions>
-    </Card>
-  );
-}
 
 const columns: GridColumns = [
   { field: 'name', headerName: 'Name', width: 300, sortable: false },
@@ -77,54 +50,32 @@ export function FollowedArtistsGenres() {
 
   return (
     <Layout>
-      <Typography variant="h5" gutterBottom>
-        Artists from liked tracks
+      <Typography variant="h5" gutterBottom sx={{ mb: 2 }}>
+        Genres from followed artists
       </Typography>
 
-      {isLoading ? (
-        <Grid container spacing={3}>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-          <Grid item xs={3}>
-            <ArtistCardSkeleton />
-          </Grid>
-        </Grid>
-      ) : (
-        <div style={{ height: 800, width: '100%' }}>
-          <DataGridPro
-            rows={data}
-            columns={columns}
-            getRowId={(a) => a.name}
-            disableColumnSelector
-            disableColumnMenu
-          />
-        </div>
-      )}
+      <Typography variant="subtitle1" gutterBottom sx={{ mb: 2 }}>
+        Here are genres
+      </Typography>
+
+      <div style={{ height: 800 }}>
+        <DataGridPro
+          loading={isLoading}
+          rows={data}
+          columns={columns}
+          getRowId={(a) => a.name}
+          disableSelectionOnClick
+          disableColumnResize
+          disableColumnMenu
+          disableColumnReorder
+          disableColumnSelector
+          disableDensitySelector
+          disableMultipleColumnsSorting
+          disableColumnFilter
+          disableMultipleColumnsFiltering
+          hideFooter
+        />
+      </div>
     </Layout>
   );
 }
