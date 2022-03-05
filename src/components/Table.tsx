fd48059e0add2728e8275f0006ad523b4383e705
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-03-05 21:58:50 +0100

    Move table styles out of component body

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,21 +1,24 @@
+import { SxProps, Theme } from '@mui/material';
 import { DataGridPro, DataGridProProps } from '@mui/x-data-grid-pro';
+
+const styles: SxProps<Theme> = {
+  background: '#fff',
+  border: 0,
+  boxShadow: 2,
+
+  '.MuiDataGrid-columnSeparator': {
+    display: 'none',
+  },
+
+  '.MuiDataGrid-columnHeaderTitleContainer': {
+    padding: 0,
+  },
+};
 
 export function Table(props: DataGridProProps) {
   return (
     <DataGridPro
-      sx={{
-        background: '#fff',
-        border: 0,
-        boxShadow: 2,
-
-        '.MuiDataGrid-columnSeparator': {
-          display: 'none',
-        },
-
-        '.MuiDataGrid-columnHeaderTitleContainer': {
-          padding: 0,
-        },
-      }}
+      sx={styles}
       disableSelectionOnClick
       disableColumnResize
       disableColumnMenu
