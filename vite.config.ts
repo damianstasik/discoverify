e9a7e923e5cd971c294e2f59be3b8550c07cb612
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-05-30 19:48:22 +0200

    Enable jotai's helper utils for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [jotaiDebugLabel, jotaiReactRefresh],
+      },
+    }),
+  ],
 });
