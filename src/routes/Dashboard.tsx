d34fb8aa3370db3a55e801b886b388f2d99e6a29
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-06-22 19:41:29 +0200

    Clean up loading skeletons in dashboard, simplify cards

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,7 +1,5 @@
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
@@ -22,6 +20,62 @@ import {
 } from '@mui/material';
 import Icon from '@mdi/react';
 import { loadingTrackPreview, tokenState, trackPreviewState } from '../store';
+
+function getRandomArbitrary(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+function ArtistSkeleton() {
+  const width = getRandomArbitrary(10, 40);
+
+  return (
+    <ListItem disableGutters>
+      <ListItemButton disableGutters>
+        <ListItemAvatar>
+          <Skeleton
+            animation="wave"
+            width={40}
+            height={40}
+            variant="circular"
+          />
+        </ListItemAvatar>
+        <ListItemText
+          primary={
+            <Skeleton animation="wave" variant="text" width={`${width}%`} />
+          }
+        />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function TrackSkeleton() {
+  const width1 = getRandomArbitrary(20, 60);
+  const width2 = getRandomArbitrary(20, 60);
+
+  return (
+    <ListItem disableGutters>
+      <ListItemAvatar sx={{ alignItems: 'center' }}>
+        <IconButton disabled aria-label="Play">
+          <Skeleton
+            animation="wave"
+            width={24}
+            height={24}
+            variant="circular"
+          />
+        </IconButton>
+      </ListItemAvatar>
+      <ListItemText
+        primary={
+          <Skeleton animation="wave" variant="text" width={`${width1}%`} />
+        }
+        secondary={
+          <Skeleton animation="wave" variant="text" width={`${width2}%`} />
+        }
+      />
+    </ListItem>
+  );
+}
 
 export default function Dashboard() {
   const token = useRecoilValue(tokenState);
@@ -54,256 +108,244 @@ export default function Dashboard() {
   return (
     <Grid container spacing={4}>
       <Grid item xs={4}>
-        <Card>
-          <CardContent>
-            <Typography variant="h5" sx={{ mb: 1.5, color: '#fff' }}>
-              Liked tracks
-            </Typography>
-            <Typography variant="h6">
-              {isLoading ? (
-                <Skeleton width="15%" />
-              ) : (
-                data?.likedTracksSpotify ?? 'N/A'
-              )}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              on Spotify
-            </Typography>
-            <Typography variant="h6">
-              {isLoading ? (
-                <Skeleton width="15%" />
-              ) : (
-                data?.likedTracks ?? 'N/A'
-              )}
-            </Typography>
-            <Typography color="text.secondary">through Discoverify</Typography>
-          </CardContent>
-          <CardActions>
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/liked"
-              disabled={isLoading}
-            >
-              All liked tracks
-            </Button>
-          </CardActions>
+        <Card sx={{ p: 2 }}>
+          <Typography variant="h5" sx={{ mb: 1.5, color: '#fff' }}>
+            Liked tracks
+          </Typography>
+          <Typography variant="h6">
+            {isLoading ? (
+              <Skeleton animation="wave" width="15%" />
+            ) : (
+              data?.likedTracksSpotify ?? 'N/A'
+            )}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            on Spotify
+          </Typography>
+          <Typography variant="h6">
+            {isLoading ? (
+              <Skeleton animation="wave" width="15%" />
+            ) : (
+              data?.likedTracks ?? 'N/A'
+            )}
+          </Typography>
+          <Typography color="text.secondary">through Discoverify</Typography>
+
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/liked"
+            disabled={isLoading}
+            sx={{ mt: 2 }}
+          >
+            All liked tracks
+          </Button>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card>
-          <CardContent>
-            <Typography variant="h5" sx={{ mb: 1.5, color: '#fff' }}>
-              Followed artists
-            </Typography>
-            <Typography variant="h6">
-              {isLoading ? (
-                <Skeleton width="15%" />
-              ) : (
-                data?.followedArtistsSpotify ?? 'N/A'
-              )}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              on Spotify
-            </Typography>
-            <Typography variant="h6">
-              {isLoading ? (
-                <Skeleton width="15%" />
-              ) : (
-                data?.followedArtists ?? 'N/A'
-              )}
-            </Typography>
-            <Typography color="text.secondary">through Discoverify</Typography>
-          </CardContent>
-          <CardActions>
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/artists"
-              disabled={isLoading}
-            >
-              All followed artists
-            </Button>
-          </CardActions>
+        <Card sx={{ p: 2 }}>
+          <Typography variant="h5" sx={{ mb: 1.5, color: '#fff' }}>
+            Followed artists
+          </Typography>
+          <Typography variant="h6">
+            {isLoading ? (
+              <Skeleton animation="wave" width="15%" />
+            ) : (
+              data?.followedArtistsSpotify ?? 'N/A'
+            )}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            on Spotify
+          </Typography>
+          <Typography variant="h6">
+            {isLoading ? (
+              <Skeleton animation="wave" width="15%" />
+            ) : (
+              data?.followedArtists ?? 'N/A'
+            )}
+          </Typography>
+          <Typography color="text.secondary">through Discoverify</Typography>
+
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/artists"
+            disabled={isLoading}
+            sx={{ mt: 2 }}
+          >
+            All followed artists
+          </Button>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card>
-          <CardContent>
-            <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
-              Recently played track
-            </Typography>
-            <Typography sx={{ mt: 1.5 }} variant="h6">
-              {isLoading ? (
-                <Skeleton />
-              ) : (
-                <Breadcrumbs sx={{ fontSize: 'inherit' }}>
-                  {data?.recentlyPlayedTrack?.track.artists.map((artist) => (
-                    <Link
-                      component={RouterLink}
-                      to={`/artist/${artist.id}`}
-                      key={artist.id}
-                    >
-                      {artist.name}
-                    </Link>
-                  ))}
-                </Breadcrumbs>
-              )}
-            </Typography>
-            <Typography variant="subtitle1">
-              {isLoading ? (
-                <Skeleton />
-              ) : (
-                <Link
-                  component={RouterLink}
-                  to={`/track/${data?.recentlyPlayedTrack?.track.id}`}
-                  key={data?.recentlyPlayedTrack?.track.id}
-                >
-                  {data?.recentlyPlayedTrack?.track.name}
-                </Link>
-              )}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/recently-played"
-              disabled={isLoading}
-            >
-              More recently played tracks
-            </Button>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item xs={4}>
-        <Card>
-          <CardContent>
-            <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
-              Top 5 artists
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              based on listening history
-            </Typography>
-            <List dense disablePadding>
-              {isLoading && (
-                <ListItem disableGutters>
-                  <ListItemButton disableGutters>
-                    <ListItemAvatar>
-                      <Skeleton width={40} height={40} variant="circular" />
-                    </ListItemAvatar>
-                    <ListItemText primary={<Skeleton variant="text" />} />
-                  </ListItemButton>
-                </ListItem>
-              )}
-              {data?.topArtists.map((artist) => (
-                <ListItem disableGutters key={artist.id}>
-                  <ListItemButton
-                    disableGutters
+        <Card sx={{ p: 2 }}>
+          <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
+            Recently played track
+          </Typography>
+          <Typography sx={{ mt: 1.5 }} variant="h6">
+            {isLoading ? (
+              <Skeleton animation="wave" />
+            ) : (
+              <Breadcrumbs sx={{ fontSize: 'inherit' }}>
+                {data?.recentlyPlayedTrack?.track.artists.map((artist) => (
+                  <Link
                     component={RouterLink}
                     to={`/artist/${artist.id}`}
+                    key={artist.id}
                   >
-                    <ListItemAvatar>
-                      <Avatar alt={artist.name} src={artist.images[0].url} />
-                    </ListItemAvatar>
-                    <ListItemText primary={artist.name} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </CardContent>
-          <CardActions>
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/top-artists"
-              disabled={isLoading}
-            >
-              All top artists
-            </Button>
-          </CardActions>
+                    {artist.name}
+                  </Link>
+                ))}
+              </Breadcrumbs>
+            )}
+          </Typography>
+          <Typography variant="subtitle1">
+            {isLoading ? (
+              <Skeleton animation="wave" />
+            ) : (
+              <Link
+                component={RouterLink}
+                to={`/track/${data?.recentlyPlayedTrack?.track.id}`}
+                key={data?.recentlyPlayedTrack?.track.id}
+              >
+                {data?.recentlyPlayedTrack?.track.name}
+              </Link>
+            )}
+          </Typography>
+
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/recently-played"
+            disabled={isLoading}
+            sx={{ mt: 2 }}
+          >
+            More recently played tracks
+          </Button>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card>
-          <CardContent>
-            <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
-              Top 5 tracks
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              based on listening history
-            </Typography>
-            <List dense disablePadding>
-              {isLoading && (
-                <ListItem disableGutters>
-                  <ListItemAvatar sx={{ alignItems: 'center' }}>
-                    <IconButton disabled aria-label="Play">
-                      <Skeleton width={24} height={24} variant="circular" />
-                    </IconButton>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={<Skeleton variant="text" />}
-                    secondary={<Skeleton variant="text" width="70%" />}
-                  />
-                </ListItem>
-              )}
-              {data?.topTracks.map((track) => (
-                <ListItem disableGutters key={track.id}>
+        <Card sx={{ p: 2 }}>
+          <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
+            Top 5 artists
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            based on listening history
+          </Typography>
+          <List dense disablePadding>
+            {isLoading && (
+              <>
+                <ArtistSkeleton />
+                <ArtistSkeleton />
+                <ArtistSkeleton />
+                <ArtistSkeleton />
+                <ArtistSkeleton />
+              </>
+            )}
+            {data?.topArtists.map((artist) => (
+              <ListItem disableGutters key={artist.id}>
+                <ListItemButton
+                  disableGutters
+                  component={RouterLink}
+                  to={`/artist/${artist.id}`}
+                >
                   <ListItemAvatar>
-                    <IconButton
-                      aria-label="Play"
-                      disabled={isLoadingTrackPreview}
-                      onClick={() => playPreview(track)}
-                      color={isCurrentlyPlaying(track) ? 'primary' : 'default'}
-                    >
-                      <Icon
-                        path={
-                          isCurrentlyPlaying(track)
-                            ? mdiPauseCircleOutline
-                            : mdiPlayCircleOutline
-                        }
-                        size={1}
-                      />
-                    </IconButton>
+                    <Avatar alt={artist.name} src={artist.images[0].url} />
                   </ListItemAvatar>
-                  <ListItemText
-                    primary={
-                      <Link
-                        component={RouterLink}
-                        to={`/track/${track.id}`}
-                        sx={{ fontSize: '1rem' }}
-                      >
-                        {track.name}
-                      </Link>
-                    }
-                    secondary={
-                      <Breadcrumbs sx={{ fontSize: 'inherit' }}>
-                        {track.artists.map((artist) => (
-                          <Link
-                            component={RouterLink}
-                            to={`/artist/${artist.id}`}
-                            key={artist.id}
-                          >
-                            {artist.name}
-                          </Link>
-                        ))}
-                      </Breadcrumbs>
-                    }
-                  />
-                </ListItem>
-              ))}
-            </List>
-          </CardContent>
-          <CardActions>
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/top-tracks"
-              disabled={isLoading}
-            >
-              All top tracks
-            </Button>
-          </CardActions>
+                  <ListItemText primary={artist.name} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/top-artists"
+            disabled={isLoading}
+            sx={{ mt: 2 }}
+          >
+            All top artists
+          </Button>
+        </Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card sx={{ p: 2 }}>
+          <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
+            Top 5 tracks
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            based on listening history
+          </Typography>
+          <List dense disablePadding>
+            {isLoading && (
+              <>
+                <TrackSkeleton />
+                <TrackSkeleton />
+                <TrackSkeleton />
+                <TrackSkeleton />
+                <TrackSkeleton />
+              </>
+            )}
+            {data?.topTracks.map((track) => (
+              <ListItem disableGutters key={track.id}>
+                <ListItemAvatar>
+                  <IconButton
+                    aria-label="Play"
+                    disabled={isLoadingTrackPreview}
+                    onClick={() => playPreview(track)}
+                    color={isCurrentlyPlaying(track) ? 'primary' : 'default'}
+                  >
+                    <Icon
+                      path={
+                        isCurrentlyPlaying(track)
+                          ? mdiPauseCircleOutline
+                          : mdiPlayCircleOutline
+                      }
+                      size={1}
+                    />
+                  </IconButton>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link
+                      component={RouterLink}
+                      to={`/track/${track.id}`}
+                      sx={{ fontSize: '1rem' }}
+                    >
+                      {track.name}
+                    </Link>
+                  }
+                  secondaryTypographyProps={{
+                    component: 'div',
+                  }}
+                  secondary={
+                    <Breadcrumbs sx={{ fontSize: 'inherit' }}>
+                      {track.artists.map((artist) => (
+                        <Link
+                          component={RouterLink}
+                          to={`/artist/${artist.id}`}
+                          key={artist.id}
+                        >
+                          {artist.name}
+                        </Link>
+                      ))}
+                    </Breadcrumbs>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/top-tracks"
+            disabled={isLoading}
+            sx={{ mt: 2 }}
+          >
+            All top tracks
+          </Button>
         </Card>
       </Grid>
     </Grid>
