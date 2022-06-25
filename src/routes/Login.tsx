6bbd7228a2538a5540bdf75d0228defde645de88
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-06-25 12:18:39 +0200

    Refactor login page (uses react-query now and has basic styling)

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,35 +1,62 @@
-import Card from '@mui/material/Card';
-import { useState } from 'react';
-import LoadingButton from '@mui/lab/LoadingButton';
+import { LoadingButton } from '@mui/lab';
+import { useMutation } from 'react-query';
+import { useSnackbar } from 'notistack';
+import { Card, Box, Typography } from '@mui/material';
 
 export function Login() {
-  const [isLoading, setLoading] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
 
-  const handleClick = () => {
-    setLoading(true);
-    fetch(`${import.meta.env.VITE_API_URL}/login`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
+  const { mutate, isLoading } = useMutation<{ url: string }, Error>(
+    async function authUrlMutation() {
+      const req = await fetch(`${import.meta.env.VITE_API_URL}/auth/url`);
+
+      if (!req.ok) {
+        throw new Error();
+      }
+
+      const res = await req.json();
+
+      return res;
+    },
+    {
+      onSuccess(res) {
         window.location.href = res.url;
-      })
-      .catch((e) => {
-        console.log('login error', e);
-        setLoading(false);
-      });
-  };
+      },
+      onError(error) {
+        console.log('Login error', error);
+
+        enqueueSnackbar('Login error', {
+          variant: 'error',
+        });
+      },
+    },
+  );
+
   return (
-    <div className="max-w-md mx-auto mt-24">
-      <Card>
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Card sx={{ maxWidth: 300, p: 3 }}>
+        <Typography variant="h5" sx={{ mb: 1.5, color: '#fff' }}>
+          Login
+        </Typography>
+        <Typography variant="body2" color="text.secondary" mb={3}>
+          You can login with Spotify by clicking on the button below.
+        </Typography>
         <LoadingButton
           variant="contained"
-          onClick={handleClick}
+          onClick={() => mutate()}
           loading={isLoading}
         >
           Login with Spotify
         </LoadingButton>
       </Card>
-    </div>
+    </Box>
   );
 }
