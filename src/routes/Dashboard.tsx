b8fe9100bb6b4bdff5913b438e454a0f7476fb83
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-06-28 22:02:28 +0200

    Optimize dashboard query

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { useQuery } from 'react-query';
+import { QueryFunction, useQuery } from 'react-query';
 import { Link as RouterLink } from 'react-router-dom';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
@@ -23,6 +23,71 @@ import { loadingTrackPreview, tokenState, trackPreviewState } from '../store';
 
 function getRandomArbitrary(min: number, max: number) {
   return Math.random() * (max - min) + min;
+}
+
+function Track({ track }) {
+  const [trackPreview, setTrackPreview] = useRecoilState(trackPreviewState);
+
+  const isLoadingTrackPreview = useRecoilValue(loadingTrackPreview);
+
+  const isCurrentlyPlaying = (t) =>
+    trackPreview?.url === t.preview_url && trackPreview?.context === t;
+
+  const playPreview = (t) =>
+    setTrackPreview({
+      url: t.preview_url,
+      context: t,
+      state: 'playing',
+    });
+
+  return (
+    <ListItem disableGutters>
+      <ListItemAvatar>
+        <IconButton
+          aria-label="Play"
+          disabled={isLoadingTrackPreview}
+          onClick={() => playPreview(track)}
+          color={isCurrentlyPlaying(track) ? 'primary' : 'default'}
+        >
+          <Icon
+            path={isCurrentlyPlaying(track) ? mdiPauseCircle : mdiPlayCircle}
+            size={1}
+          />
+        </IconButton>
+      </ListItemAvatar>
+      <ListItemText
+        primary={
+          <Link
+            component={RouterLink}
+            to={`/track/${track.id}`}
+            sx={{ fontSize: '1rem' }}
+            color="#fff"
+            underline="hover"
+          >
+            {track.name}
+          </Link>
+        }
+        secondaryTypographyProps={{
+          component: 'div',
+        }}
+        secondary={
+          <Breadcrumbs sx={{ fontSize: 'inherit' }}>
+            {track.artists.map((artist) => (
+              <Link
+                component={RouterLink}
+                to={`/artist/${artist.id}`}
+                key={artist.id}
+                color="rgba(255, 255, 255, 0.7)"
+                underline="hover"
+              >
+                {artist.name}
+              </Link>
+            ))}
+          </Breadcrumbs>
+        }
+      />
+    </ListItem>
+  );
 }
 
 function ArtistSkeleton() {
@@ -77,38 +142,41 @@ function TrackSkeleton() {
   );
 }
 
+interface StatsResponse {
+  likedTracksSpotify: number;
+  likedTracks: number;
+  followedArtistsSpotify: number;
+  followedArtists: number;
+  topTracks: any[];
+  recentlyPlayedTrack: any;
+  topArtists: any[];
+}
+
+const statsQuery: QueryFunction<StatsResponse, string[]> = async ({
+  queryKey,
+  signal,
+}) => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/user/stats`, {
+    signal,
+    headers: {
+      Authorization: `Bearer ${queryKey[1]}`,
+    },
+  });
+
+  const body = await res.json();
+
+  return body;
+};
+
 export default function Dashboard() {
   const token = useRecoilValue(tokenState);
 
-  const { data, isLoading } = useQuery(['stats'], async function statsQuery() {
-    const res = await fetch(`${import.meta.env.VITE_API_URL}/user/stats`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const body = await res.json();
-
-    return body;
-  });
-
-  const [trackPreview, setTrackPreview] = useRecoilState(trackPreviewState);
-  const isLoadingTrackPreview = useRecoilValue(loadingTrackPreview);
-
-  const isCurrentlyPlaying = (track) =>
-    trackPreview?.url === track.preview_url && trackPreview?.context === track;
-
-  const playPreview = (track) =>
-    setTrackPreview({
-      url: track.preview_url,
-      context: track,
-      state: 'playing',
-    });
+  const { data, isLoading } = useQuery(['stats', token], statsQuery);
 
   return (
     <Grid container spacing={4}>
       <Grid item xs={4}>
-        <Card sx={{ p: 2 }}>
+        <Card sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="h5" sx={{ mb: 1.5, color: '#fff' }}>
             Liked tracks
           </Typography>
@@ -136,14 +204,14 @@ export default function Dashboard() {
             component={RouterLink}
             to="/liked"
             disabled={isLoading}
-            sx={{ mt: 2, px: 1, ml: -1 }}
+            sx={{ mt: 2, px: 1, py: 0.5, ml: -1 }}
           >
             All liked tracks
           </Button>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card sx={{ p: 2 }}>
+        <Card sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="h5" sx={{ mb: 1.5, color: '#fff' }}>
             Followed artists
           </Typography>
@@ -171,65 +239,39 @@ export default function Dashboard() {
             component={RouterLink}
             to="/artists"
             disabled={isLoading}
-            sx={{ mt: 2, px: 1, ml: -1 }}
+            sx={{ mt: 2, px: 1, py: 0.5, ml: -1 }}
           >
             All followed artists
           </Button>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card sx={{ p: 2 }}>
-          <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
+        <Card sx={{ p: 2, pb: 1.5 }}>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{ color: '#fff', mb: 1.5 }}
+          >
             Recently played track
           </Typography>
-          <Typography sx={{ mt: 1.5 }} variant="subtitle1">
-            {isLoading ? (
-              <Skeleton animation="wave" />
-            ) : (
-              <Breadcrumbs sx={{ fontSize: 'inherit' }}>
-                {data?.recentlyPlayedTrack?.track.artists.map((artist) => (
-                  <Link
-                    component={RouterLink}
-                    to={`/artist/${artist.id}`}
-                    key={artist.id}
-                    color="#fff"
-                    underline="hover"
-                  >
-                    {artist.name}
-                  </Link>
-                ))}
-              </Breadcrumbs>
-            )}
-          </Typography>
-          <Typography variant="subtitle2">
-            {isLoading ? (
-              <Skeleton animation="wave" />
-            ) : (
-              <Link
-                component={RouterLink}
-                to={`/track/${data?.recentlyPlayedTrack?.track.id}`}
-                key={data?.recentlyPlayedTrack?.track.id}
-                color="rgba(255, 255, 255, 0.7)"
-                underline="hover"
-              >
-                {data?.recentlyPlayedTrack?.track.name}
-              </Link>
-            )}
-          </Typography>
+          <List dense disablePadding>
+            {isLoading && <TrackSkeleton />}
+            {!isLoading && <Track track={data?.recentlyPlayedTrack?.track} />}
+          </List>
 
           <Button
             size="small"
             component={RouterLink}
             to="/recently-played"
             disabled={isLoading}
-            sx={{ mt: 2, px: 1, ml: -1 }}
+            sx={{ mt: 2, px: 1, py: 0.5, ml: -1 }}
           >
             More recently played tracks
           </Button>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card sx={{ p: 2 }}>
+        <Card sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
             Top 5 artists
           </Typography>
@@ -268,14 +310,14 @@ export default function Dashboard() {
             component={RouterLink}
             to="/top-artists"
             disabled={isLoading}
-            sx={{ mt: 2, px: 1, ml: -1 }}
+            sx={{ mt: 2, px: 1, py: 0.5, ml: -1 }}
           >
             All top artists
           </Button>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card sx={{ p: 2 }}>
+        <Card sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="h5" component="div" sx={{ color: '#fff' }}>
             Top 5 tracks
           </Typography>
@@ -293,56 +335,7 @@ export default function Dashboard() {
               </>
             )}
             {data?.topTracks.map((track) => (
-              <ListItem disableGutters key={track.id}>
-                <ListItemAvatar>
-                  <IconButton
-                    aria-label="Play"
-                    disabled={isLoadingTrackPreview}
-                    onClick={() => playPreview(track)}
-                    color={isCurrentlyPlaying(track) ? 'primary' : 'default'}
-                  >
-                    <Icon
-                      path={
-                        isCurrentlyPlaying(track)
-                          ? mdiPauseCircle
-                          : mdiPlayCircle
-                      }
-                      size={1}
-                    />
-                  </IconButton>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={
-                    <Link
-                      component={RouterLink}
-                      to={`/track/${track.id}`}
-                      sx={{ fontSize: '1rem' }}
-                      color="#fff"
-                      underline="hover"
-                    >
-                      {track.name}
-                    </Link>
-                  }
-                  secondaryTypographyProps={{
-                    component: 'div',
-                  }}
-                  secondary={
-                    <Breadcrumbs sx={{ fontSize: 'inherit' }}>
-                      {track.artists.map((artist) => (
-                        <Link
-                          component={RouterLink}
-                          to={`/artist/${artist.id}`}
-                          key={artist.id}
-                          color="rgba(255, 255, 255, 0.7)"
-                          underline="hover"
-                        >
-                          {artist.name}
-                        </Link>
-                      ))}
-                    </Breadcrumbs>
-                  }
-                />
-              </ListItem>
+              <Track track={track} key={track.id} />
             ))}
           </List>
 
@@ -351,7 +344,7 @@ export default function Dashboard() {
             component={RouterLink}
             to="/top-tracks"
             disabled={isLoading}
-            sx={{ mt: 2, px: 1, ml: -1 }}
+            sx={{ mt: 2, px: 1, py: 0.5, ml: -1 }}
           >
             All top tracks
           </Button>
