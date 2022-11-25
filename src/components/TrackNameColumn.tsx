0a30330f3875a3a744a7c072176225f5b3893891
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2022-11-25 19:35:29 +0100

    Specify value type for TrackNameColumn

--- src/components/TrackNameColumn.tsx
+++ src/components/TrackNameColumn.tsx
@@ -2,7 +2,7 @@ import { Link } from '@mui/material';
 import { CellContext } from '@tanstack/react-table';
 import { Link as RouterLink } from 'react-router-dom';
 
-export const TrackNameColumn = (props: CellContext<any, any>) => {
+export const TrackNameColumn = (props: CellContext<any, string>) => {
   return (
     <Link
       component={RouterLink}
