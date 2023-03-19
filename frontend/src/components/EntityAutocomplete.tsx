390710acc5d9c3638558e12f3fcbff1b1ab73f15
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-03-19 23:03:23 +0100

    Add pointer cursor to autocomplete items

--- frontend/src/components/EntityAutocomplete.tsx
+++ frontend/src/components/EntityAutocomplete.tsx
@@ -17,7 +17,7 @@ const Option = ({ item }: { item: Item }) => {
         <Combobox.Option
           className={({ active }) =>
             twMerge(
-              'text-white px-3 py-2 flex items-center gap-3',
+              'text-white px-3 py-2 flex items-center gap-3 cursor-pointer',
               active && 'bg-neutral-750',
             )
           }
@@ -37,7 +37,7 @@ const Option = ({ item }: { item: Item }) => {
         <Combobox.Option
           className={({ active }) =>
             twMerge(
-              'text-white px-3 py-2 flex items-center gap-3',
+              'text-white px-3 py-2 flex items-center gap-3 cursor-pointer',
               active && 'bg-neutral-750',
             )
           }
@@ -51,7 +51,7 @@ const Option = ({ item }: { item: Item }) => {
         <Combobox.Option
           className={({ active }) =>
             twMerge(
-              'text-white px-3 py-2 flex items-center gap-3',
+              'text-white px-3 py-2 flex items-center gap-3 cursor-pointer',
               active && 'bg-neutral-750',
             )
           }
