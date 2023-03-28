a7d4da59ba3e2450b230c51424589bbc8bd2d77b
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-03-28 21:12:32 +0200

    Use proper color for track/artist chip skeleton

--- frontend/src/components/TrackChipSkeleton.tsx
+++ frontend/src/components/TrackChipSkeleton.tsx
@@ -1,10 +1,10 @@
 export const TrackChipSkeleton = () => {
   return (
     <div className="h-12 px-2 bg-slate-500 rounded-md flex-shrink-0 flex items-center gap-2">
-      <span className="rounded s-8 animate-pulse bg-slate-500" />
+      <span className="rounded s-8 animate-pulse bg-slate-450" />
       <div className="flex justify-center flex-col gap-1">
-        <span className="rounded text-sm h-em w-32 animate-pulse bg-slate-500" />
-        <span className="rounded text-xs h-em w-24 animate-pulse bg-slate-500" />
+        <span className="rounded text-sm h-em w-32 animate-pulse bg-slate-450" />
+        <span className="rounded text-xs h-em w-24 animate-pulse bg-slate-450" />
       </div>
     </div>
   );
