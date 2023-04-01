232fd254ef8f4e8b6bc3db1b0a0b24ae27cfdf57
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-04-01 10:34:08 +0200

    Experiment with color extraction

--- frontend/src/components/BgImg.tsx
+++ frontend/src/components/BgImg.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 import { tw } from '../tw';
 
 interface Props {
@@ -7,24 +7,28 @@ interface Props {
   className?: string;
 }
 
-export function BgImg({ src, className, alt }: Props) {
-  const [loaded, setLoaded] = useState(false);
+export const BgImg = forwardRef<HTMLImageElement, Props>(
+  ({ src, className, alt }, ref) => {
+    const [loaded, setLoaded] = useState(false);
 
-  return (
-    <div
-      className={tw(
-        'absolute top-0 inset-x-0 z-0 h-[500px] opacity-0 transition-opacity duration-500',
-        loaded && 'opacity-20',
-        className,
-      )}
-    >
-      <span className="absolute inset-0 z-0 bg-gradient-to-b from-transparent to-slate-900" />
-      <img
-        src={src}
-        alt={alt}
-        className="object-cover w-full h-full"
-        onLoad={() => setLoaded(true)}
-      />
-    </div>
-  );
-}
+    return (
+      <div
+        className={tw(
+          'absolute top-0 inset-x-0 z-0 h-[500px] opacity-0 transition-opacity duration-500',
+          loaded && 'opacity-20',
+          className,
+        )}
+      >
+        <span className="absolute inset-0 z-0 bg-gradient-to-b from-transparent to-slate-900" />
+        <img
+          src={src}
+          alt={alt}
+          className="object-cover w-full h-full"
+          onLoad={() => setLoaded(true)}
+          ref={ref}
+          crossOrigin="anonymous"
+        />
+      </div>
+    );
+  },
+);
